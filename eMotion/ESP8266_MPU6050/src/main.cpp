bda2b8a7025f7ddef2c96d39758bf69139a21bb2
brolin <brolin108@gmail.com>
2017-06-03 18:37:06 -0500

    Agrega salida OUTPUT_READABLE_YAWPITCHROLL

--- eMotion/ESP8266_MPU6050/src/main.cpp
+++ eMotion/ESP8266_MPU6050/src/main.cpp
@@ -10,7 +10,9 @@
 // requires I2Cdev library: https://github.com/jrowberg/i2cdevlib
 #include "I2Cdev.h"
 // requires MPU-6050 part of the I2Cdev lib: https://github.com/jrowberg/i2cdevlib/tree/master/Arduino/MPU6050
-#include "MPU6050.h"
+//#include "MPU6050.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
 
 ///////////////////////
 // MPU6050 Setup //
@@ -24,13 +26,38 @@ MPU6050 accelgyro;
 // uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
 // list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
 // not so easy to parse, and slow(er) over UART.
-#define OUTPUT_READABLE_ACCELGYRO
+//#define OUTPUT_READABLE_ACCELGYRO
 
 // uncomment "OUTPUT_BINARY_ACCELGYRO" to send all 6 axes of data as 16-bit
 // binary, one right after the other. This is very fast (as fast as possible
 // without compression or data loss), and easy to parse, but impossible to read
 // for a human.
 //#define OUTPUT_BINARY_ACCELGYRO
+
+// uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
+// pitch/roll angles (in degrees) calculated from the quaternions coming
+// from the FIFO. Note this also requires gravity vector calculations.
+// Also note that yaw/pitch/roll angles suffer from gimbal lock (for
+// more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
@@ -55,12 +82,11 @@ void sendBundleViaOSC();
 
 void getGyro();
 void getAccel();
+void getMpuData();
 
 void sendViaOSC();
 
 void setup() {
-
-
 
   pinMode(0, OUTPUT);
   digitalWrite(0, HIGH);
@@ -82,6 +108,43 @@ void setup() {
   // initialize device
   Serial.println("Initializing I2C devices...");
   accelgyro.initialize();
+
+
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = accelgyro.dmpInitialize();
+  // supply your own gyro offsets here, scaled for min sensitivity
+  accelgyro.setXGyroOffset(220);
+  accelgyro.setYGyroOffset(76);
+  accelgyro.setZGyroOffset(-85);
+  accelgyro.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        accelgyro.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        mpuIntStatus = accelgyro.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = accelgyro.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
 
   // verify connection
   Serial.println("Testing device connections...");
@@ -121,11 +184,13 @@ void loop() {
   }
   if (sendCount > 1000)
   {
-    getGyro();  // Print ?G: gx, gy, gz?
-    getAccel(); // Print ?A: ax, ay, az?
+    //getGyro();  // Print ?G: gx, gy, gz?
+    //getAccel(); // Print ?A: ax, ay, az?
+
+    getMpuData();
 
     //sendViaOSC();
-    sendBundleViaOSC();
+    //sendBundleViaOSC();
   }
 }
 
@@ -161,10 +226,46 @@ void sendBundleViaOSC() {
   // Serial.println(aX);
 }
 
+void getMpuData() {
+  mpuIntStatus = accelgyro.getIntStatus();
 
+  // get current FIFO count
+  fifoCount = accelgyro.getFIFOCount();
 
-void getGyro()
-{
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    // reset so we can continue cleanly
+    accelgyro.resetFIFO();
+    Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  } else if (mpuIntStatus & 0x02) {
+    // wait for correct available data length, should be a VERY short wait
+    while (fifoCount < packetSize) fifoCount = accelgyro.getFIFOCount();
+
+    // read a packet from FIFO
+    accelgyro.getFIFOBytes(fifoBuffer, packetSize);
+        
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+    // display Euler angles in degrees
+    accelgyro.dmpGetQuaternion(&q, fifoBuffer);
+    accelgyro.dmpGetGravity(&gravity, &q);
+    accelgyro.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    Serial.print("ypr\t");
+    Serial.print(ypr[0] * 180/M_PI);
+    Serial.print("\t");
+    Serial.print(ypr[1] * 180/M_PI);
+    Serial.print("\t");
+    Serial.println(ypr[2] * 180/M_PI);
+#endif
+  }
+}
+
+void getGyro() {
   accelgyro.getRotation(&gx, &gy, &gz);
   Serial.print("gx:");
   Serial.println(gx);
